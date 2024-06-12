5f4ff2ba0dcaa546b383435d49293a596726d018
curryworry <curryworry13@gmail.com>
2024-06-12 19:24:59 +1200

    Created Course component with Header and Parts

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -20,30 +20,39 @@ const Content = ({ parts }) =>
     />      
   </>
 
-const App = () => {
-  const course = 'Half Stack application development'
-  const parts = [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      name: 'State of a component',
-      exercises: 14
-    }
-  ]
+const Course = ({course}) => {
+  console.log(course);
+  return(
+  <div>
+    <Header course={course.name}/>
+    <Content parts={course.parts}/>
+  </div>  
+)
+}
 
-  return (
-    <div>
-      <Header course={course} />
-      <Content parts={parts} />
-      <Total sum={parts[0].exercises + parts[1].exercises + parts[2].exercises} />
-    </div>
-  )
+const App = () => {
+  const course = {
+    id: 1,
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      }
+    ]
+  }
+  return <Course course={course}/>
 }
 
 export default App
