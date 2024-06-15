8d7ffc108e426b9ba33e376bcae82e09ebf87ade
curryworry <curryworry13@gmail.com>
2024-06-15 22:36:13 +1200

    2.11 Notes read from json-server using axios, id key added for all new entries

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,4 +1,5 @@
-import { useState } from 'react'
+import { useState,useEffect } from 'react'
+import axios from 'axios'
 
 const Filter = ({changeHandler}) => {
   return(<div>
@@ -30,17 +31,24 @@ const Display = ({filteredPersons}) => {
 }
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
+  const [persons, setPersons] = useState([])
+
   const [filteredPersons, setFilteredPersons] = useState(persons)
 
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   //const [search, setSearch] = useState('')
+
+  useEffect(()=>{
+    axios
+    .get('http://localhost:3001/persons')
+    .then(response=>{
+      console.log('promise fulfilled')
+      setPersons(response.data)
+      setFilteredPersons(response.data)
+    })
+    }
+    ,[])
 
   const handleSearch = (e) => {
     let search = e.target.value;
@@ -70,7 +78,7 @@ const App = () => {
     const existingUser = newPersons.some(person=>person.name===newName)
     existingUser
       ?alert(`${newName} has already been added to phone book`)
-      :setPersons(newPersons.concat({name: newName,number: newNumber})),setFilteredPersons(newPersons.concat({name: newName,number: newNumber}))
+      :setPersons(newPersons.concat({name: newName,number: newNumber,id:`${persons.length+1}`})),setFilteredPersons(newPersons.concat({name: newName,number: newNumber,id:`${filteredPersons.length+1}`}))
   }
 
     return (
